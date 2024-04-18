700263bfc6ad431065bf3c4c9d7c623c127cf816
zhzluke96 <zhz961101@gmail.com>
2024-04-18 15:56:51 +0800

    :sparkles: manually merge code / fakeInspect /

---
+++ src/fakeInspect.ts
@@ -0,0 +1,8 @@
+// lark-js generated code need to be extracted
+if (!(globalThis as any).util) {
+  (globalThis as any).util = {
+    inspect: (obj: any, options?: any) => {
+      return JSON.stringify(obj);
+    },
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "./fakeInspect";
 export * from "./PromptParser";
 export * from "./types";
 export * from "./compilation";
