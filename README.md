42a2a83cd9d8c5c96652a44683282f04ddc8defd
zhzluke96 <zhz961101@gmail.com>
2024-05-15 18:01:45 +0800

    :memo: update

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ const output = parser.parse(prompt1);
 
 output
 ```
-[{"type":"plain","value":"masterpiece"},{"type":"plain","value":"1girl"},{"type":"plain","value":"blonde hair"},{"type":"extra_networks","value":"lora","args":["Zelda_v1","0.5"]},{"type":"weighted","value":"chromatic aberration","args":["0.7"]},{"type":"plain","value":"sharp focus"},{"type":"plain","value":"hyper detailed"},{"type":"weighted","value":"fog","args":["0.7"]},{"type":"extra_networks","value":"hypernet","args":["sxz-bloom","0.5"]},{"type":"negative","value":"real photo","args":[1]},{"type":"scheduled_full","value":"0.9","args":[[{"type":"plain","value":"highlight"}],[{"type":"plain","value":"dark"}]]},{"type":"positive","value":"good anatomy","args":[3]}]
+[{"type":"plain","value":"masterpiece"},{"type":"plain","value":"1girl"},{"type":"plain","value":"blonde hair"},{"type":"extra_networks","value":"lora","args":["Zelda_v1","0.5"]},{"type":"weighted","value":0.7,"args":[{"type":"plain","value":"chromatic aberration"}]},{"type":"plain","value":"sharp focus"},{"type":"plain","value":"hyper detailed"},{"type":"weighted","value":0.7,"args":[{"type":"plain","value":"fog"}]},{"type":"extra_networks","value":"hypernet","args":["sxz-bloom","0.5"]},{"type":"negative","value":1,"args":[{"type":"plain","value":"real photo"}]},{"type":"scheduled_full","value":0.9,"args":[[{"type":"plain","value":"highlight"}],[{"type":"plain","value":"dark"}]]},{"type":"positive","value":3,"args":[{"type":"plain","value":"good anatomy"}]}]
 ```
 
 # build
@@ -36,8 +36,6 @@ pip install lark-js
 ```
 pnpm build-lark
 ```
-
-> NOTE: two options values need to be cleared manually (`strict` & `ordered_sets`)
 
 ## 2. build package
 ```
--- tests/simple_demo.ts
+++ tests/simple_demo.ts
@@ -4,9 +4,9 @@ import { generation_token, PromptParser } from "../src/main";
 
 const parser = new PromptParser();
 
-// const prompt1 = `masterpiece, 1girl, blonde hair, <lora:Zelda_v1:0.5>, (chromatic aberration:0.7), sharp focus, hyper detailed, (fog:0.7), <hypernet:sxz-bloom:0.5>, [real photo], [highlight:dark:0.9], (((good anatomy)))`;
+const prompt1 = `masterpiece, 1girl, blonde hair, <lora:Zelda_v1:0.5>, (chromatic aberration:0.7), sharp focus, hyper detailed, (fog:0.7), <hypernet:sxz-bloom:0.5>, [real photo], [highlight:dark:0.9], (((good anatomy)))`;
 // const prompt1 = `<lora:detailed_eye:0.5>, <lora:LORA MODEL:0.2>, kpop, asian 18 years old girl with pony hair, sbg, breast grab, breast lift, nipples, <lora:SelfBreastGrab:0.8>, couple, hetero, nude girl standing behind girl, make up, <lora:aiKorea_sd15:1>`;
-const prompt1 = `(best quality, highest quality), 1girl, mature, (((hair over eyes, short pink and black hair, bob cut, wavy hair, curly hair, multicolor hair))), looking at viewer, (((turtleneck, oversized hoodie, shorts, sleeves_past_fingers, ong_sleeves, sleeves_past_wrists, fake animal ears, zipper_pull_tab, zipper))), (((cyberpunk))), rating:safe, solo, black eyes, cyber eyes, eye protesys, ((flat chest)), black background,  <lora:CyberPunkAI:1>, <lora:hair_over_eyes:1>, <lora:style:0.7> ,<lora:holographic-v1.0:0.7>`;
+// const prompt1 = `(best quality, highest quality), 1girl, mature, (((hair over eyes, short pink and black hair, bob cut, wavy hair, curly hair, multicolor hair))), looking at viewer, (((turtleneck, oversized hoodie, shorts, sleeves_past_fingers, ong_sleeves, sleeves_past_wrists, fake animal ears, zipper_pull_tab, zipper))), (((cyberpunk))), rating:safe, solo, black eyes, cyber eyes, eye protesys, ((flat chest)), black background,  <lora:CyberPunkAI:1>, <lora:hair_over_eyes:1>, <lora:style:0.7> ,<lora:holographic-v1.0:0.7>`;
 const tokens = parser.parse(prompt1);
 
 // console.log("tokens: ", tokens);
